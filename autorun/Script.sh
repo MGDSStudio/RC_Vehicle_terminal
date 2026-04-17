7ed54f5bf8daf589992c22257de33eea842b4443
MGDSStudio <mgdsstudio@yahoo.com>
2026-04-17 07:28:36 +0300

    no

---
+++ autorun/Script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo systemctl stop pigpiod
+
+sudo killall -9 RC_Vehicle_terminal pigpiod 2>/dev/null
+
+sudo rm -f /var/run/pigpio.pid &&
+
+cd RC_Vehicle_terminal &&
+
+git pull &&
+
+cd build &&
+
+cmake -DCMAKE_BUILD_TYPE=Release .. &&
+
+cmake --build . -j 1 &&
+
+sudo ./RC_Vehicle_terminal
+
+
+
+
+
+#
