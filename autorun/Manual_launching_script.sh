3f945d54108af73bcb76b8d32ef428377ccdb81c
MGDSStudio <mgdsstudio@yahoo.com>
2026-04-20 09:39:52 +0300

    No

---
+++ autorun/Manual_launching_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo systemctl stop pigpiod
+sudo killall -9 RC_Vehicle_terminal pigpiod 2>/dev/null
+sudo rm -f /var/run/pigpio.pid &&
+cd RC_Vehicle_terminal &&
+cd build &&
+sudo ./RC_Vehicle_terminal > output.log
+
+
+
+
+
+
+#
--- autorun/Script.sh
+++ autorun/Script.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-sudo systemctl stop pigpiod
-sudo killall -9 RC_Vehicle_terminal pigpiod 2>/dev/null
-sudo rm -f /var/run/pigpio.pid &&
+sleep 5
+export DISPLAY=:0
+#export SDL_VIDEODRIVER=dummy  #
+#export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+systemctl stop pigpiod
+killall -9 RC_Vehicle_terminal pigpiod 2>/dev/null
+rm -f /var/run/pigpio.pid
+cd /home/pi/RC_Vehicle_terminal/build || exit
+./RC_Vehicle_terminal > /home/pi/output.log 2>&1
 
-cd RC_Vehicle_terminal &&
-cd build &&
-sudo ./RC_Vehicle_terminal > output.log
-
-
+#crontab -e
+#@reboot /bin/bash /home/pi/start_vehicle.sh > /home/pi/cron_log.log 2>&1
 
 
 
